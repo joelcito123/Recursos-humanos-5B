2a8a0dac4dbdcedcb5534020e45301a6217697c4
Luis <100473510+carlos8788@users.noreply.github.com>
2024-02-11 07:07:12 -0300

    delete node_modules

---
+++ backend/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO profesiones
+	(nombre) 
+VALUES
+	('Abogado'),
+    ('Administrador'),
+	('Arquitecto'),
+	('Botánico'),
+	('Computista'),
+	('Economista'),
+	('Técnico de sonido'),
+	('Profesor'),
+	('Linguista')
+;
+
+INSERT INTO aspirantes
+	(dni,nombre,apellido,email,telefono,linkedinUrl,fechaNac,sexo, imagen,profesionId)
+VALUES
+(1,'Gloria','Medina','gm@gmail.com','12345','url',"1999-2-12",1,'url',2),
+(2,'Daniel','Fuentes','df@gmail.com','12345','url',"1998-1-12",2,'url',7),
+(3,'Tim','Tim','tt@gmail.com','12345','url',"1997-8-15",3,'url',9),
+(4,'Rocio','Carle','rc@gmail.com','12345','url',"1995-11-25",1,'url',8),
+(5,'Victor','Fuentes','vf@gmail.com','12345','url',"1996-7-3",2,'url',5),
+(6,'Luis','Fuentes','lf@gmail.com','12345','url',"1995-8-9",3,'url',6)
