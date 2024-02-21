ce2c479208186f709b9cc8f715e783765e8728fd
Luis <100473510+carlos8788@users.noreply.github.com>
2024-02-21 08:25:30 -0300

    fix img aspirantes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
-// configure express
+
 const corsOptions = {
     origin: 'http://localhost:3001',
     credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-
+app.use('/public', express.static(path.join(__dirname, 'upload', 'img')))
 
 const aspirantesRoutesApi = require("./routes/aspirantesRoutesApi");
 const profesionesRoutesApi = require("./routes/profesionesRoutesApi");
--- front/src/components/Aspirantes.jsx
+++ front/src/components/Aspirantes.jsx
@@ -1,11 +1,5 @@
 import { faComment, faEnvelope, faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import foto1 from '../assets/img/foto1.jpg'
-import foto2 from '../assets/img/foto2.jpg'
-import foto3 from '../assets/img/foto3.jpg'
-import foto4 from '../assets/img/foto4.jpg'
-import foto5 from '../assets/img/foto5.jpg'
-import foto6 from '../assets/img/foto6.jpg'
 import { useEffect, useState } from 'react';
 import { getAspirantes } from '../api/api';
 
@@ -17,15 +11,6 @@ const Aspirantes = () => {
         getAspirantes().then(result => setAspirantes(result.data.payload));
     }, [])
 
-    // const aspirantes = [
-
-    //     { id: 1, nombre: 'Gloria Medina', posicion: 'Administrador', imagen: foto1 },
-    //     { id: 2, nombre: 'Daniel Fuentes', posicion: 'Técnico de sonido', imagen: foto2 },
-    //     { id: 3, nombre: 'Tim Tim', posicion: 'Linguista', imagen: foto3 },
-    //     { id: 4, nombre: 'Rocio Carle', posicion: 'Profesor', imagen: foto4 },
-    //     { id: 5, nombre: 'Victor Fuentes', posicion: 'Computista', imagen: foto5 },
-    //     { id: 6, nombre: 'Luis Fuentes', posicion: 'Economista', imagen: foto6 },
-    // ];
     return (
         <section className="content aspirantes">
             <h2>Aspirantes</h2>
@@ -33,7 +18,7 @@ const Aspirantes = () => {
                 {aspirantes.map(aspirante => (
                     <div key={aspirante.id} className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
                         <div className="box-avatar">
-                            <img src={`assets/img/foto${aspirante.imagen}`} alt={aspirante.nombre} />
+                            <img src={`http://localhost:3000/public/foto${aspirante.imagen}`} alt={aspirante.nombre} />
                         </div>
                         <div className="box-bio">
                             <h2 className="bio-name">{aspirante.nombre}</h2>
