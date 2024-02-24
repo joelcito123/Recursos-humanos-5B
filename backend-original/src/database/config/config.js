c770687207312500f289236ee68cbaefa0c6c775
joel <joelagustin239@gmail.com>
2024-02-24 15:56:23 -0300

    database

--- backend-original/src/database/config/config.js
+++ backend-original/src/database/config/config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "recruitingRH",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
---
+++ backend-original/src/database/models/Aspirante.js
@@ -0,0 +1,62 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Aspirantes";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        dni: {
+            type: dataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+        },
+        nombre: {
+            type: dataTypes.STRING(20),
+            allowNull: false,
+        },
+        apellido: {
+            type: dataTypes.STRING(20),
+            allowNull: false,
+        },
+        email: {
+            type: dataTypes.STRING(30),
+            allowNull: false,
+        },
+        telefono: {
+            type: dataTypes.INTEGER,
+        },
+        linkedinUrl: {
+            type: dataTypes.STRING(255),
+        },
+        fechaNac: {
+            type: dataTypes.DATE,
+        },
+        sexo: {
+            type: dataTypes.ENUM('Femenino', 'Masculino', 'Otro'),
+            allowNull: false,
+        },
+        imagen: {
+            type: dataTypes.STRING(20),
+        },
+        profesionId: {
+            type: dataTypes.INTEGER,
+        }
+    };
+    let config = {
+        tableName: "aspirantes",
+        timestamps: false
+    }
+    
+    const Aspirante = sequelize.define(alias, cols, config);
+
+    Aspirante.associate = function(models) {
+        Aspirante.belongsTo(models.Profesiones, {
+            as: "profesiones",
+            foreignKey: "profesionId",
+        })
+    }
+
+    return Aspirante;
+}
