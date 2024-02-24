2bc9f5247b9c050fa7b36eb72ea333d419fc0e3e
ramox-mq <daniel.rsi6oa@gmail.com>
2024-02-24 16:57:07 -0300

    cors

--- backend-original/app.js
+++ backend-original/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3080;
+const cors = require('cors');
 
 const aspirantesRoutes = require('./src/routes/aspirantesRoutes');
 const profesionesRoutes = require('./src/routes/profesionesRoutes');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', aspirantesRoutes);
 app.use('/api', profesionesRoutes);
